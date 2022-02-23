9defc2d5d1baad28a12fdd0bb342ee3cd171ba59
mk812y <mk812y@gmail.com>
2022-02-23 20:38:25 +0300

    Initial Commit

---
+++ StateAndDataFlow_3_3/StateAndDataFlow_3_3App.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlow_3_3App.swift
+//  StateAndDataFlow_3_3
+//
+//  Created by Михаил Куприянов on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlow_3_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
